d4118cf10b7e1e16ef02251fe86f966551c5e851
admin <admin@DESKTOP-D655F3H>
2020-04-10 14:41:06 +0900

    테스트용 파일입니다.2

--- src/test2/test3.java
+++ src/test2/test3.java
@@ -1,10 +1,10 @@
 package test2;
 
-public class test2 {
+public class test3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hi");
+System.out.println("bye");
 	}
 
 }
